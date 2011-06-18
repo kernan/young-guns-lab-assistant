970ebf66f7eed5f82f3bd566d6b12e800c0ee701
kylepetrovich@gmail.com <kylepetrovich@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-06-18 21:15:21 +0000

    Completed the calc method.

--- M4/src/edu/gatech/oad/antlab/person/Person2.java
+++ M4/src/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,12 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.Random;
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
- * @version 1.1
+ * @author Kyle Petrovich
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -30,8 +30,19 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+            String oldstring = input;
+            System.out.println(oldstring);
+            String newstring = "";
+            Random randomizer = new Random();
+            int position = 0;
+            while (oldstring.length()!=0){
+                //Pick a random character position, put the character there into the newstring and remove it.
+                position = randomizer.nextInt(oldstring.length());
+                newstring += oldstring.charAt(position);
+                oldstring  = oldstring.substring(0,position) + oldstring.substring(position+1,oldstring.length());
+                //iterate until the old string is depleted.
+            }
+	  return newstring;
 	}
 	/**
 	 * Return a string rep of this object
