5be5c57bc99381d54cccbf8542fd24062759b465
kylepetrovich@gmail.com <kylepetrovich@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-06-25 20:17:16 +0000

    Due to the complications dynamic scaffolding would bring when applied to our roles system, I decided it wouldn't be too much harder to just make our own, plus there's the added benefit of not having the blackbox scaffold system to figure out. Anyways, this code currently is ignoring any security checks but they are in place and can easily be recommented. The create page and list courses page (or should I say views/action combinations) are working on a rudimentary level as of now. I will "beautify" these in the next day or maybe tonight.

---
+++ LabAssistant/grails-app/controllers/edu/gatech/youngguns/labassistant/CourseController.groovy
@@ -0,0 +1,48 @@
+package edu.gatech.youngguns.labassistant
+
+class CourseController {
+
+	/**
+	* Dependency injection for the springSecurityService.
+	*/
+   def springSecurityService
+
+   /**
+	* Default action; If the user is not logged in, redirects to login, otherwise displays
+	* all courses.
+	*/
+   def index = {
+	   //if (springSecurityService.isLoggedIn()) {
+		   redirect action:'list'
+	   //}
+	   //else {
+		   //redirect(controller:'login',action:'auth')
+	   //}
+   }
+   /**
+    * List all courses.
+    */
+   def list = {
+	   //TODO: Add "join" link next to each one if Student.
+	   return [courseList: Course.list(), courseTotal: Course.count()]
+	   
+   }
+   
+   /**
+    * Display create course page if user has proper permissions.
+    */
+   def create = {
+	   //TODO: Restrict this page to instructors and Sysadmins only.
+	   return
+   }
+   
+   /**
+    * Save course object from form if coming from create, then redirect to list.
+    */
+   def save = {
+	   //TODO: Redirect to list if HTTPrequest didn't come from create?
+	   def course = new Course(params)
+	   course.save()
+	   redirect action:'list'
+   }
+}
