0f9d6e393b18420c14eba17fc7c85f22e5b93983
rkernan@gmail.com <rkernan@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-06-26 19:35:29 +0000

    Added Course, but it doesn't work correctly yet

---
+++ trunk/LabAssistant/test/unit/edu/gatech/youngguns/labassistant/StudentTests.groovy
@@ -0,0 +1,18 @@
+package edu.gatech.youngguns.labassistant
+
+
+import grails.test.*
+
+class StudentTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
