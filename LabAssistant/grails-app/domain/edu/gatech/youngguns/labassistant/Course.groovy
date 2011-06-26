024f82f44a2b68454e108dd4af3bb19def53fbfb
rkernan@gmail.com <rkernan@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-06-26 19:35:29 +0000

    Added Course, but it doesn't work correctly yet

--- LabAssistant/grails-app/domain/edu/gatech/youngguns/labassistant/Course.groovy
+++ LabAssistant/grails-app/domain/edu/gatech/youngguns/labassistant/Course.groovy
@@ -1,15 +1,40 @@
 package edu.gatech.youngguns.labassistant
 
+import org.apache.commons.lang.builder.HashCodeBuilder
+
 class Course {
-
+	
 	String name
-
+	User instructor
+	Set students
+	Set labs
+	
+	/**
+	 * constraints for Course params
+	 */
 	static constraints = {
-		name blank: false, unique: true
+		name(blank: false, unique: true)
 	}
-
+	
+	/**
+	 * mapping for tables containing Courses
+	 */
 	static mapping = {
-		name column: 'coursename'
+		name column: "'name'"
+		instructor column: "'instructor'"
 	}
-
+	
+	/**
+	 * checks equality based on Course name
+	 */
+	boolean equals(other) {
+		return this.name.equals(other.getName())
+	}
+	
+	/**
+	 * outputs Course name
+	 */
+	String toString() {
+		return this.name
+	}
 }
---
+++ LabAssistant/test/unit/edu/gatech/youngguns/labassistant/StudentControllerTests.groovy
@@ -0,0 +1,17 @@
+package edu.gatech.youngguns.labassistant
+
+import grails.test.*
+
+class StudentControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
