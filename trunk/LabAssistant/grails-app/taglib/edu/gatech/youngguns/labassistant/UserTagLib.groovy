e9821823406974357a0c24aeba7788d1c574f2b2
rkernan@gmail.com <rkernan@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-07-05 03:02:15 +0000

    added taglib to display the current user's name at login

---
+++ trunk/LabAssistant/grails-app/taglib/edu/gatech/youngguns/labassistant/UserTagLib.groovy
@@ -0,0 +1,17 @@
+package edu.gatech.youngguns.labassistant
+
+/**
+ * 
+ * @author Robert Kernan
+ *
+ */
+
+class UserTagLib {
+	
+	def springSecurityService
+	
+	def getUserName = { attrs, body ->
+		def currentUser = User.get(springSecurityService.principal.id)
+		out << currentUser.name
+	}
+}
