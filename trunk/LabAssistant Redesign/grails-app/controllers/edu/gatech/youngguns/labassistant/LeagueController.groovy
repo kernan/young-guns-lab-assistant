d1fca63d0b9c54decde9c1d8b7cd7c3dedeea8b3
milkman107@gmail.com <milkman107@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-07-30 04:16:11 +0000

    Completed addTeam.

--- trunk/LabAssistant Redesign/grails-app/controllers/edu/gatech/youngguns/labassistant/LeagueController.groovy
+++ trunk/LabAssistant Redesign/grails-app/controllers/edu/gatech/youngguns/labassistant/LeagueController.groovy
@@ -3,15 +3,54 @@ package edu.gatech.youngguns.labassistant
 import grails.plugins.springsecurity.Secured
 
 class LeagueController {
-    
+     
     /**
-     * adds a new team
-     * @author Philip Smith
-     * Secured: logged in fully, roles: Player
-     */
-    @Secured(["IS_AUTHENTICATED_FULLY", "ROLE_PLAYER"])
-    def addTeam = {
-        
-    }
+    * Render the create team page
+    * @author Philip Smith
+    * Secured: logged in fully, roles: Player
+    */
+   @Secured(["IS_AUTHENTICATED_FULLY", "ROLE_PLAYER"])
+   def createTeam = {
+       render(view: 'createTeam', model: [league: League.findById(params['league'])])
+   }
+
+   /**
+    * Adds a new team
+    * @author Philip Smith
+    * Secured: logged in fully, roles: Player
+    */
+   @Secured(["IS_AUTHENTICATED_FULLY", "ROLE_PLAYER"])
+   def addTeam = {
+       League league = League.findById(params['league'])
+       String name = params['name']
+       String captain = params['captain']
+       
+       if (!league) {
+           redirect(action: error, message: "invalid league")
+       }
+       if (!name) {
+           redirect(action: error, message: "invalid name")
+       }
+       if (!captain){
+           redirect(action: error, message: "invalid captain")
+       }
+       Team team = new Team(name: name, league: league)
+       team.assignCaptain(captain)
+       
+       for(player in params['players']) {
+           team.join(player)
+       }
+     
+       team.save()
+       redirect(action:success)
+   }
+  
+   def error = {
+       redirect(action: list)
+   }
+   
+   def success = {
+       redirect(action: list)
+   }
 
 }
