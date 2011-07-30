49ef9eef9e423215d358df6e71aadbcfdff8b078
kylepetrovich@gmail.com <kylepetrovich@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-07-30 03:53:03 +0000

    Lolmethods and test cases. I don't know how to Junit so...yea.

--- LabAssistant Redesign/grails-app/controllers/edu/gatech/youngguns/labassistant/SportController.groovy
+++ LabAssistant Redesign/grails-app/controllers/edu/gatech/youngguns/labassistant/SportController.groovy
@@ -11,6 +11,26 @@ class SportController {
      */
     @Secured(["IS_AUTHENTICATED_FULLY", "ROLE_EMPLOYEE"])
     def addLeague = {
-        
+            User currentUser = springSecurityService.currentUser
+            if (currentUser.hasRole("ROLE_EMPLOYEE")) {
+                    String name = params['name']
+                    Sport sport = params['sport']
+                    if (!name) {
+                        redirect(action: error, message: "invalid name")
+                    }
+                    if (!sport){
+                        redirect(action: error, message: "invalid sport")
+                    }
+                    def league = new League(name: name, sport: sport)
+                    league.save()
+                    redirect(action:success);
+            } 
+    }
+    def error = {
+        redirect(action: list)
+    }
+    
+    def success = {
+        redirect(action: list)
     }
 }
--- LabAssistant Redesign/test/integration/edu/gatech/youngguns/labassistant/SportControllerTests.groovy
+++ LabAssistant Redesign/test/integration/edu/gatech/youngguns/labassistant/SportControllerTests.groovy
@@ -17,6 +17,38 @@ class SportControllerTests extends GrailsUnitTestCase {
      * @author Kyle Petrovich
      */
     void testAddLeague() {
+        //setup user
+        User user = new User(name: "User 1", username: "user1", password: "blahblahblah")
+        mockDomain(User, [user])
+        //setup domain objects
+        Sport sport = new Sport(name: "Sport 1", maxTeamSize: 20)
+        mockDomain(Sport, [sport])
+        //save domain objects
+        user.save(failOnError: 'true')
+        sport.save(failOnError: 'true')
+        //setup controller
+        mockController(SportController)
+        def controller = new SportController()
+        controller.params.sport = sport.id
+        controller.params.name = "Bush League"
         
+        controller.addLeague()
+        assertEquals(controller.success, controller.redirectArgs.action)
     }
+    void testBlankName() {
+        controller.params.name = ''
+        controller.params.sport = sport.id
+        controller.addLeague()
+        assertEquals('/error', controller.response.redirectedUrl)
+    }
+
+    void testAssignCaptainInvalidUser() {
+        controller.params.team = 'farts'
+        controller.params.sport = 4
+        controller.addLeague()
+        assertNull(Sport.findById(4), sport)
+        assertEquals('/error', controller.response.redirectedUrl)
+    }
+    
+
 }
