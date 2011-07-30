c765285e67dd86b195cd3497649940cb106f08f4
milkman107@gmail.com <milkman107@gmail.com@0cd993d1-05fb-5e0a-5b6a-42f80b96bd7d>
2011-07-30 05:04:51 +0000

    Added unit testing for LeagueController. Specifically, the addTeam method.

--- LabAssistant Redesign/test/integration/edu/gatech/youngguns/labassistant/LeagueControllerTests.groovy
+++ LabAssistant Redesign/test/integration/edu/gatech/youngguns/labassistant/LeagueControllerTests.groovy
@@ -2,7 +2,7 @@ package edu.gatech.youngguns.labassistant
 
 import grails.test.*
 
-class LeagueControllerTests extends GrailsUnitTestCase {
+class LeagueControllerTests extends ControllerUnitTestCase {
     protected void setUp() {
         super.setUp()
     }
@@ -14,9 +14,47 @@ class LeagueControllerTests extends GrailsUnitTestCase {
     //tests
     
     /**
-     * @author Philip Smith
-     */
-    void testAddTeam() {
-        
-    }
+    * @author Philip Smith
+    */
+   void testAddTeam() {
+       controller.params.league= '1'
+       controller.params.captain = '2'
+       controller.params.name = 'Saul Goode'
+       controller.addTeam()
+       assertEquals(controller.success, controller.redirectArgs.action)
+   }
+    
+    /**
+    * @author Philip Smith
+    */
+   void testAddTeamInvalidLeague() {
+       controller.params.league= ''
+       controller.params.captain = '2'
+       controller.params.name = 'HAY'
+       controller.addTeam()
+       assertEquals(controller.error, controller.redirectArgs.action)
+   }
+
+
+   /**
+    * @author Philip Smith
+    */
+   void testAddTeamInvalidName() {
+       controller.params.league= '1'
+       controller.params.captain = '2'
+       controller.params.name = ''
+       controller.addTeam()
+       assertEquals(controller.error, controller.redirectArgs.action)
+   }
+
+   /**
+    * @author Philip Smith
+    */
+   void testAddTeamInvalidCaptain() {
+       controller.params.league= '1'
+       controller.params.captain = ''
+       controller.params.name = 'Saul Goode'
+       controller.addTeam()
+       assertEquals(controller.error, controller.redirectArgs.action)
+   }
 }
